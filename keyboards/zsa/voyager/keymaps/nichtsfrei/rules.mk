c8a0aaa8da36b54099f54652b0827a8da04904af
Philipp Eder <philipp.eder@posteo.net>
2024-07-29 10:12:56 +0200

    Add: my keyboard layut

---
+++ keyboards/zsa/voyager/keymaps/nichtsfrei/rules.mk
@@ -0,0 +1,11 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+CAPS_WORD_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+ORYX_ENABLE = yes
+RGB_MATRIX_CUSTOM_KB = no
+TAP_DANCE_ENABLE = no
+SPACE_CADET_ENABLE = no
+COMBO_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
