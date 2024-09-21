94b56b33481f447c18ac32b027d77ffec055ac2c
Steam Deck User <deck@steamdeck.lan>
2024-09-21 12:18:07 +0200

    Switch to layer and 36 keys layout for testing

--- keyboards/zsa/voyager/keymaps/nichtsfrei/keymap.c
+++ keyboards/zsa/voyager/keymaps/nichtsfrei/keymap.c
@@ -6,23 +6,19 @@
 //     ST_MACRO_0 = SAFE_RANGE,
 // };
 
-#define ENTER MT(MOD_LCTL, KC_ENTER)
+#define ENTER MT(MOD_LGUI, KC_ENTER)
 #define SPACE MT(MOD_LSFT, KC_SPACE)
-#define TAB MT(MOD_LGUI, KC_TAB)
+#define TAB MT(MOD_LCTL, KC_TAB)
 #define BSPC MT(MOD_LALT, KC_BSPC)
 
-#define MOD_A MT(MOD_LCTL, KC_A)
-#define MOD_S MT(MOD_LALT, KC_S)
-#define MOD_D MT(MOD_LGUI, KC_D)
-#define MOD_F MT(MOD_MEH, KC_F)
-#define MOD_G MT(MOD_HYPR, KC_G)
-#define MOD_H MT(MOD_HYPR, KC_H)
-#define MOD_J MT(MOD_MEH, KC_J)
-#define MOD_K MT(MOD_LGUI, KC_K)
-#define MOD_L MT(MOD_LALT, KC_L)
-#define MOD_SEMICOLON MT(MOD_LCTL, KC_SEMICOLON)
 // clang-format off
+// num home row layer - z = \, i = ', o = [, p = ], . = -, / = =, q = `,
+// symbol home row layer - shift + num, z = |, i = ", o = {, p = }, . = _, , = +, q = ~ = maybe not necessary as it is just shifted, probably better to create a specific german layer?
+// F.. homw row layer with multimedia? - o = F11, p = F12, n = NXT_T, u = V_UP, i = V_DOWN, m = PRV_T, y = PAUSE,
+// navigation home row layer, a = KC_END, s = PG_UP, d = PD_DWN, f = PG_HOME, h = KC_LEFT, j = KC_DOWN, k = KC_UP, l = KC_RIGHT
+// mouse home row layer = a = left click, s = right click, d = middle click, y = scroll left, u = scroll down, k = scroll up, l = scroll right
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // TODO: when familiar disable additional keys
   [0] = LAYOUT_voyager(
     KC_EQUAL,  KC_1, KC_2, KC_3, KC_4, KC_5 ,   KC_6, KC_7, KC_8, KC_9, KC_0,KC_MINUS,
     KC_RBRC,KC_Q, KC_W, KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
@@ -31,69 +27,83 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     SPACE, TAB                        ,   BSPC,ENTER
   ),
   [1] = LAYOUT_voyager(
-    KC_TRNS, KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,                  KC_F6,KC_F7,KC_F8,KC_F9,KC_F10, KC_F11,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_GRAVE, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_QUOTE, KC_LBRC, KC_RBRC, KC_NO,
+    KC_NO, KC_1 , KC_2 , KC_3 , KC_4 , KC_5  ,    KC_6 , KC_7 , KC_8 , KC_9 , KC_0 , KC_NO,
+    KC_NO, KC_BSLS, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_EQUAL, KC_MINUS, KC_NO,
+    KC_TRNS, KC_TRNS,                             KC_TRNS, KC_TRNS
   ),
   [2] = LAYOUT_voyager(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_MEDIA_PREV_TRACK, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_TRNS
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_END, KC_PGUP, KC_PGDN, KC_HOME, KC_NO,          KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_TRNS, KC_TRNS,                                              KC_TRNS, KC_TRNS
   ),
   [3] = LAYOUT_voyager(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, MOD_A, MOD_S, MOD_D, MOD_F, MOD_G,                    MOD_H, MOD_J, MOD_K, MOD_L, MOD_SEMICOLON, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_MEDIA_PLAY_PAUSE, KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN, KC_F11, KC_F12, KC_NO,
+    KC_NO, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5  ,    KC_F6 , KC_F7 , KC_F8 , KC_F9 , KC_F10 , KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_MEDIA_NEXT_TRACK, KC_MEDIA_PREV_TRACK, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_TRNS, KC_TRNS,                             KC_TRNS, KC_TRNS
+  ),
+  [4] = LAYOUT_voyager(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                                      QK_MOUSE_WHEEL_LEFT, QK_MOUSE_WHEEL_DOWN, QK_MOUSE_WHEEL_UP, QK_MOUSE_WHEEL_RIGHT, KC_NO, KC_NO,
+    KC_NO, QK_MOUSE_BUTTON_1, QK_MOUSE_BUTTON_2, QK_MOUSE_BUTTON_3, QK_MOUSE_BUTTON_4, QK_MOUSE_BUTTON_5,          QK_MOUSE_CURSOR_LEFT, QK_MOUSE_CURSOR_DOWN, QK_MOUSE_CURSOR_UP, QK_MOUSE_CURSOR_RIGHT, KC_NO, KC_NO,
+    KC_NO, QK_MOUSE_ACCELERATION_0, QK_MOUSE_ACCELERATION_1, QK_MOUSE_ACCELERATION_2, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_TRNS, KC_TRNS,                                              KC_TRNS, KC_TRNS
+  ),
+  // [2] = LAYOUT_voyager(
+  //   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  //   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_MEDIA_PREV_TRACK, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE, KC_TRNS,
+  //   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS,
+  //   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TRNS, KC_TRNS,
+  //   KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_TRNS
+  // ),
+  [5] = LAYOUT_voyager(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_TRNS, KC_TRNS,                                              KC_TRNS, KC_TRNS
   ),
 
 }; // clang-format on
-const uint16_t PROGMEM combo_enter_f[]  = {ENTER, KC_F, COMBO_END};
-const uint16_t PROGMEM combo_enter_mod_f[]  = {ENTER, MOD_F, COMBO_END};
-const uint16_t PROGMEM combo_enter_s[]  = {ENTER, KC_S, COMBO_END};
-const uint16_t PROGMEM combo_enter_mod_s[]  = {ENTER, MOD_S, COMBO_END};
-const uint16_t PROGMEM combo_enter_a[]  = {ENTER, KC_A, COMBO_END};
-const uint16_t PROGMEM combo_enter_mod_a[]  = {ENTER, MOD_A, COMBO_END};
+//#$%^&
+
+const uint16_t PROGMEM combo_l_num[] = {KC_J, KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_l_num_b[] = {KC_7, KC_8, KC_9, COMBO_END};
+const uint16_t PROGMEM combo_l_sym[] = {KC_S, KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_l_sym_b[] = {KC_PGUP, KC_PGDN, KC_HOME, COMBO_END};
+const uint16_t PROGMEM combo_l_fun[] = {KC_J, KC_K, KC_L, KC_SEMICOLON, COMBO_END};
+const uint16_t PROGMEM combo_l_fun_b[] = {KC_F7, KC_F8, KC_F9, KC_F10, COMBO_END};
+const uint16_t PROGMEM combo_l_mse[] = {KC_A, KC_S, KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_l_mse_b[] = {QK_MOUSE_BUTTON_1, QK_MOUSE_BUTTON_2, QK_MOUSE_BUTTON_3, QK_MOUSE_BUTTON_4, COMBO_END};
+
+
 const uint16_t PROGMEM combo_z_scolon[] = {KC_Z, KC_SEMICOLON, COMBO_END};
-const uint16_t PROGMEM combo_z_mod_scolon[] = {KC_Z, MOD_SEMICOLON, COMBO_END};
 const uint16_t PROGMEM combo_a_scolon[] = {KC_A, KC_SEMICOLON, COMBO_END};
-const uint16_t PROGMEM combo_a_mod_scolon[] = {MOD_A, MOD_SEMICOLON, COMBO_END};
 const uint16_t PROGMEM combo_d_h[]      = {KC_D, KC_H, COMBO_END};
-const uint16_t PROGMEM combo_d_mod_h[]      = {MOD_D, MOD_H, COMBO_END};
 const uint16_t PROGMEM combo_d_l[]      = {KC_D, KC_L, COMBO_END};
-const uint16_t PROGMEM combo_d_mod_l[]      = {MOD_D, MOD_L, COMBO_END};
+const uint16_t PROGMEM combo_minus[] = {KC_SEMICOLON, KC_SLASH, COMBO_END};
 
 const uint16_t PROGMEM combo_space_spc_enter[] = {SPACE, ENTER, COMBO_END};
-const uint16_t PROGMEM combo_lbrc_minus[]      = {KC_MINUS, KC_LBRC, COMBO_END};
-const uint16_t PROGMEM cmb_rbracket[]          = {KC_P, KC_LBRC, COMBO_END};
-const uint16_t PROGMEM cmb_ws[]                = {KC_S, KC_W, COMBO_END};
-const uint16_t PROGMEM cmb_ws_mod[]                = {MOD_S, KC_W, COMBO_END};
-
 combo_t key_combos[] = {
-    COMBO(combo_enter_f, TG(1)),
-    COMBO(combo_enter_mod_f, TG(1)),
-    COMBO(combo_enter_s, OSL(2)),
-    COMBO(combo_enter_mod_s, OSL(2)),
-    COMBO(combo_enter_a, TG(3)),
-    COMBO(combo_enter_mod_a, TG(3)),
+    COMBO(combo_l_num, TG(1)),
+    COMBO(combo_l_num_b, TG(1)),
+    COMBO(combo_l_sym, TG(2)),
+    COMBO(combo_l_sym_b, TG(2)),
+    COMBO(combo_l_fun, TG(3)),
+    COMBO(combo_l_fun_b, TG(3)),
+    COMBO(combo_l_mse, TG(4)),
+    COMBO(combo_l_mse_b, TG(4)),
     COMBO(combo_z_scolon, KC_ESCAPE),
-    COMBO(combo_z_mod_scolon, KC_ESCAPE),
     COMBO(combo_a_scolon, KC_ENTER),
-    COMBO(combo_a_mod_scolon, KC_ENTER),
     COMBO(combo_d_h, KC_BSPC),
-    COMBO(combo_d_mod_h, KC_BSPC),
     COMBO(combo_d_l, KC_DELETE),
-    COMBO(combo_d_mod_l, KC_DELETE),
-    COMBO(cmb_rbracket, KC_RBRC),
-    COMBO(cmb_ws, KC_SPACE),
-    COMBO(cmb_ws_mod, KC_SPACE),
+    COMBO(combo_minus, KC_MINUS), // very useful to not have to switch layer
     COMBO(combo_space_spc_enter, QK_CAPS_WORD_TOGGLE),
-    COMBO(combo_lbrc_minus, KC_EQUAL),
 };
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
@@ -107,7 +117,11 @@ void keyboard_post_init_user(void) {
 }
 
 #define NORMAL {0, 0, 69}
-#define MOD {42,42,42}
+#define MOD {42,42, 69}
+#define NUM {210, 210, 69}
+#define SYM {245, 245, 69}
+#define FUN {255, 255, 69}
+#define OUT {0, 0, 0}
 // clang-format off
 const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
     [0] = {
@@ -125,47 +139,81 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
       NORMAL, NORMAL
     },
     [1] = {
-      {0,0,0}, {195,246,120}, {195,246,120}, {195,246,120}, {195,246,120}, {195,246,120},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0},
-      {195,246,120}, {195,246,120}, {195,246,120}, {195,246,120}, {195,246,120}, {195,246,120},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {195,246,120},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0}
+      // left side
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, NUM, OUT, OUT, OUT, OUT,
+      OUT, NUM, NUM, NUM, NUM, NUM,
+      OUT, NUM, OUT, OUT, OUT, OUT,
+      OUT, OUT,
+      // right side
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, NUM, NUM, NUM, OUT,
+      NUM, NUM, NUM, NUM, NUM, OUT,
+      OUT, OUT, OUT, NUM, NUM, OUT,
+      OUT, OUT
     },
 
     [2] = {
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
-      {51,255,255}, {51,255,255}, {51,255,255}, {51,255,255}, {51,255,255}, {0,0,0},
-      {0,153,153}, {0,153,153}, {0,153,153}, {0,153,153}, {0,0,0}, {0,0,0},
-      {0,204,204}, {0,204,204}, {0,204,204}, {0,204,204}, {0,0,0}, {0,0,0},
-      {0,0,0}, {0,0,0}
+      // left side
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, SYM, SYM, SYM, SYM, SYM,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT,
+      // right side
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      SYM, SYM, SYM, SYM, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT
     },
 
     [3] = {
       // left side
-      NORMAL, NORMAL, NORMAL, NORMAL, NORMAL, NORMAL,
-      NORMAL, NORMAL, NORMAL, NORMAL, NORMAL, NORMAL,
-      NORMAL, MOD, MOD, MOD, MOD, MOD,
-      NORMAL, NORMAL, NORMAL, NORMAL, NORMAL, NORMAL,
-      NORMAL, NORMAL,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, FUN, FUN, FUN, FUN, FUN,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT,
       // right side
-      NORMAL, NORMAL, NORMAL, NORMAL, NORMAL, NORMAL,
-      NORMAL, NORMAL, NORMAL, NORMAL, NORMAL, NORMAL,
-      MOD, MOD, MOD, MOD, MOD, NORMAL,
-      NORMAL, NORMAL, NORMAL, NORMAL, NORMAL, NORMAL,
-      NORMAL, NORMAL
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      FUN, FUN, FUN, FUN, FUN, OUT,
+      FUN, FUN, FUN, FUN, FUN, OUT,
+      FUN, FUN, OUT, OUT, OUT, OUT,
+      OUT, OUT
+    },
+    [4] = {
+      // left side
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, MOD, MOD, MOD, MOD, MOD,
+      OUT, MOD, MOD, MOD, OUT, OUT,
+      OUT, OUT,
+      // right side
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      MOD, MOD, MOD, MOD, OUT, OUT,
+      MOD, MOD, MOD, MOD, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT
+    },
+
+    [5] = {
+      // left side
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT,
+      // OUT OUT
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT, OUT, OUT, OUT, OUT,
+      OUT, OUT
     },
 
 };
+
 
 // clang-format on
 void set_layer_color(int layer) {
@@ -201,6 +249,9 @@ bool rgb_matrix_indicators_user(void) {
             break;
         case 3:
             set_layer_color(3);
+            break;
+        case 4:
+            set_layer_color(4);
             break;
         default:
             if (rgb_matrix_get_flags() == LED_FLAG_NONE) rgb_matrix_set_color_all(0, 0, 0);
