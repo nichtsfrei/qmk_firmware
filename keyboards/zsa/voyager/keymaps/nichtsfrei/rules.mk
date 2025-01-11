3a75ddbddef83f71b03cb087c83d358a75e48475
Philipp Eder <philipp.eder@posteo.net>
2025-01-11 17:51:45 +0100

    Add raw hid, remove TG in favor of manual layer handling
    Instead of using TG I use
    ```c
    
    void process_combo_event(uint16_t combo_index, bool pressed) {
        if (combo_index >= 0 && combo_index <= 3 && pressed) {
            if (get_highest_layer(layer_state) == combo_index + 1) {
                layer_move(0);
            } else {
                layer_move(combo_index + 1);
            }
         }
    }
    ```
    
    to disable all other layers when switching. That way I don't have to
    recreate my layer steps to go back to one and don't have to define
    multiple TO combos.
    
    Additionally I enabled raw hid so that I can get the layer via an
    user space program.

--- keyboards/zsa/voyager/keymaps/nichtsfrei/rules.mk
+++ keyboards/zsa/voyager/keymaps/nichtsfrei/rules.mk
@@ -2,7 +2,8 @@
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
-ORYX_ENABLE = yes
+RAW_ENABLE = yes
+ORYX_ENABLE = no
 RGB_MATRIX_CUSTOM_KB = no
 COMBO_ENABLE = yes
 MOUSEKEY_ENABLE = yes
